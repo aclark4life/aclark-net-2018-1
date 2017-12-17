25b768f4a5c61c471c8f7f40f2c031df788d5e9d
Alex Clark <aclark@aclark.net>
2017-12-17 16:46:05 -0500

    Update

--- aclarknet/settings.py
+++ aclarknet/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'aclarknet.www',
+    'captcha',
+    'crispy_forms',
+    'django_ses',
 ]
 
 MIDDLEWARE = [
