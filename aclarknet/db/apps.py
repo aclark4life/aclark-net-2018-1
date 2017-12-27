3e89f7be67ad83fab4af36f55d4cc54a6f698675
Alex Clark <aclark@aclark.net>
2017-12-27 18:12:06 -0500

    Update

--- aclarknet/db/apps.py
+++ aclarknet/db/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from django.contrib.auth.signals import user_logged_in
 
 
 class DBConfig(AppConfig):
     name = 'aclarknet.db'
+
+    def ready(self):
+        from .signals import login_receiver
+        user_logged_in.connect(login_receiver)
