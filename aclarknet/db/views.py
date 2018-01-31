96daa2da75514b077a9eb85d568ec6b1ad179917
Alex Clark <aclark@aclark.net>
2018-01-31 08:23:52 -0500

    Update

--- aclarknet/db/views.py
+++ aclarknet/db/views.py
@@ -747,7 +747,7 @@ def time_index(request):
         },
         filter_by={
             'estimate': None,
-            # 'invoice__isnull': False,
+            'user__isnull': False,
         },
         order_by=(
             'invoiced',
